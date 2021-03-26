c9a767fdaed94105dc6f30219ad69a413fd4dd71
CarlosEduardoGui <eduardocarlos456@gmail.com>
2021-03-26 13:15:47 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.LeilaoOnline.Testes/LeilaoTeste.cs
@@ -0,0 +1,46 @@
+﻿using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Testes
+{
+    public class LeilaoTeste
+    {
+        //Existe um fato
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 800);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 900);
+
+            leilao.TerminaPregao();
+
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void LeilaoComApenasUmLance()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            leilao.TerminaPregao();
+
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
